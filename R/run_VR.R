19ee5484fb01e78084183652c8f9f7ea3f503d3f
abidecoskunsetirek <abidecoskun38@gmail.com>
2024-06-26 12:33:43 +0200

    Update run_VR.R

--- R/run_VR.R
+++ R/run_VR.R
@@ -5,25 +5,33 @@ source("R/set_VR_environment.R")
 coords <- plot_3Dbag_buildings(5.387200, 52.155170, 100)
 
 # Save 3D model as glb file
-#save_model_as_glb("inst/","buildings", coords)
-save_model_as_glb("inst/","buildings", coords, "gray")
+save_model_as_glb("inst/","buildings", coords)
 
+# Save 3D model as glb file with a specific color
+#save_model_as_glb("inst/","buildings", coords, "gray")
 
 # Create 3D model for A-frame
-#create_VR("inst/buildings.glb", "output.html")
-create_VR("inst/buildings.glb", "output.html",c(0, 2.5, -3),c(0.01, 0.01, 0.01),c(-75,0,0))
+create_VR("inst/buildings.glb", "output.html")
 
-# set environment: Example usage
-#set_VR_environment("output.html", "tron")
+# Create 3D model for A-frame with specific position, scale, and rotation values
+#create_VR("inst/buildings.glb", "output.html",c(0, 2.5, -3),c(0.01, 0.01, 0.01),c(-75,0,0))
+
+# Set VR environment
 #Valid values: none, default, contact, egypt, checkerboard, forest, goaland, yavapai, goldmine, threetowers, poison, arches, tron, japan, dream, volcano, starry, osiris
-set_VR_environment("output.html", "tron", skyType = "color", skyColor = "pink")
+set_VR_environment("output.html", "tron")
+
+# Set VR environment with sky type (color/gradient) and color
+#set_VR_environment("output.html", "tron", skyType = "color", skyColor = "pink")
 #set_VR_environment("output.html", "japan", skyType = "gradient", skyColor = "#FFDD99")
 
-# rotate VR model: Example usage
-#rotate_VR("output.html", TRUE)
-rotate_VR("output.html", TRUE, speed = 0.0002, clockwise = 0)
-#rotate_VR("output.html", FALSE)
+# Rotate VR model clockwise with default speed
+rotate_VR("output.html", TRUE)
 
+# Rotate VR model clockwise/counterclockwise with a specific speed
+#rotate_VR("output.html", TRUE, speed = 0.0002, clockwise = 0)
+
+# Stop the VR model rotation
+#rotate_VR("output.html", FALSE)
 
 
 
