eaa36caa92e2ac48f50631b931f1b7e834d32641
abidecoskunsetirek <abidecoskun38@gmail.com>
2024-06-26 21:07:57 +0200

    Update run_VR.R

--- R/run_VR.R
+++ R/run_VR.R
@@ -1,11 +1,11 @@
-source("R/save_model_as_glb.R")
+source("R/save_model.R")
 source("R/set_VR_environment.R")
 
 # Process and plot 3D data
 coords <- plot_3Dbag_buildings(5.387200, 52.155170, 100)
 
-# Save 3D model as glb file
-save_model_as_glb("inst/","buildings", coords)
+# Save 3D model as obj and glb file
+save_model("inst/","buildings", coords)
 
 # Save 3D model as glb file with a specific color
 #save_model_as_glb("inst/","buildings", coords, "gray")
@@ -16,7 +16,8 @@ create_VR("inst/buildings.glb", "output.html")
 # Create 3D model for A-frame with specific position, scale, and rotation values
 #create_VR("inst/buildings.glb", "output.html",c(0, 2.5, -3),c(0.01, 0.01, 0.01),c(-75,0,0))
 
-# Set VR environment (Valid values: none, default, contact, egypt, checkerboard, forest, goaland, yavapai, goldmine, threetowers, poison, arches, tron, japan, dream, volcano, starry, osiris)
+# Set VR environment
+#Valid values: none, default, contact, egypt, checkerboard, forest, goaland, yavapai, goldmine, threetowers, poison, arches, tron, japan, dream, volcano, starry, osiris
 set_VR_environment("output.html", "tron")
 
 # Set VR environment with sky type (color/gradient) and color
@@ -31,7 +32,3 @@ rotate_VR("output.html", TRUE)
 
 # Stop the VR model rotation
 #rotate_VR("output.html", FALSE)
-
-
-
-
