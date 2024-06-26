1d2db11a21e2a893aa9b6a8bd06819855f6c4b03
abidecoskunsetirek <abidecoskun38@gmail.com>
2024-06-26 23:03:14 +0200

    Update run_VR.R

--- R/run_VR.R
+++ R/run_VR.R
@@ -4,11 +4,11 @@ source("R/set_VR_environment.R")
 # Process and plot 3D data
 coords <- plot_3Dbag_buildings(5.387200, 52.155170, 100)
 
-# Save 3D model as obj and glb file
+# Save 3D model as obj and glb files
 save_model("inst/","buildings", coords)
 
-# Save 3D model as glb file with a specific color
-#save_model_as_glb("inst/","buildings", coords, "gray")
+# Save 3D model as obj and glb files with a specific color
+#save_model("inst/","buildings", coords, "gray")
 
 # Create 3D model for A-frame
 create_VR("inst/buildings.glb", "output.html")
@@ -20,15 +20,15 @@ create_VR("inst/buildings.glb", "output.html")
 #Valid values: none, default, contact, egypt, checkerboard, forest, goaland, yavapai, goldmine, threetowers, poison, arches, tron, japan, dream, volcano, starry, osiris
 set_VR_environment("output.html", "tron")
 
-# Set VR environment with sky type (color/gradient) and color
+# Set VR environment with sky type (color, gradient) and color
 #set_VR_environment("output.html", "tron", skyType = "color", skyColor = "pink")
 #set_VR_environment("output.html", "japan", skyType = "gradient", skyColor = "#FFDD99")
 
-# Rotate VR model clockwise with default speed
-rotate_VR("output.html", TRUE)
+# Rotate only glb models clockwise with default speed
+#rotate_VR("output.html", TRUE)
 
-# Rotate VR model clockwise/counterclockwise with a specific speed
-#rotate_VR("output.html", TRUE, speed = 0.0002, clockwise = 0)
+# Rotate only glb models clockwise/counterclockwise with a specific speed
+rotate_VR("output.html", TRUE, speed = 0.0002, clockwise = 0)
 
-# Stop the VR model rotation
+# Stop the glb model rotation
 #rotate_VR("output.html", FALSE)
