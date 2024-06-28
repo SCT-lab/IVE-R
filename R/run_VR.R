4f4a5e293fca91439f8b935d9aa44c14fe334fcc
abidecoskunsetirek <abidecoskun38@gmail.com>
2024-06-28 02:07:29 +0200

    Update run_VR.R

--- R/run_VR.R
+++ R/run_VR.R
@@ -1,27 +1,33 @@
 source("R/save_model.R")
 source("R/set_VR_environment.R")
 
-# Process and plot 3D data
-coords <- plot_3Dbag_buildings(5.387200, 52.155170, 100)
+# Define area of interest
+bbox=define_aoi(5.387200, 52.155170, 100)
+
+# Get 3DBag building data and extract coordinates
+coords=get_3Dbag_items(bbox)
+
+# Plot 3D buildings
+plot_3Dbag_buildings(coords)
 
 # Save 3D model as obj and glb files
-save_model("inst/","buildings", coords)
+#save_model("inst/","buildings", coords)
 
 # Save 3D model as obj and glb files with a specific color
-#save_model("inst/","buildings", coords, "gray")
+save_model("inst/","buildings", coords, "gray")
 
 # Create 3D model for A-frame
-create_VR("inst/buildings.glb", "output.html")
+#create_VR("inst/buildings.glb", "output.html")
 
 # Create 3D model for A-frame with specific position, scale, and rotation values
-#create_VR("inst/buildings.glb", "output.html",c(0, 2.5, -3),c(0.01, 0.01, 0.01),c(-75,0,0))
+create_VR("inst/buildings.glb", "output.html",c(0, 2.5, -3),c(0.01, 0.01, 0.01),c(-75,0,0))
 
 # Set VR environment
 #Valid values: none, default, contact, egypt, checkerboard, forest, goaland, yavapai, goldmine, threetowers, poison, arches, tron, japan, dream, volcano, starry, osiris
-set_VR_environment("output.html", "tron")
+#set_VR_environment("output.html", "tron")
 
 # Set VR environment with sky type (color, gradient) and color
-#set_VR_environment("output.html", "tron", skyType = "color", skyColor = "pink")
+set_VR_environment("output.html", "tron", skyType = "color", skyColor = "pink")
 #set_VR_environment("output.html", "japan", skyType = "gradient", skyColor = "#FFDD99")
 
 # Rotate only glb models clockwise with default speed
